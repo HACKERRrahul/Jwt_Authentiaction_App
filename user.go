8905ca8df7a41d891e1c6a83a3a9092420bedb6d
HACKERRrahul <78462372+HACKERRrahul@users.noreply.github.com>
2024-07-09 12:36:12 +0530

    Update user.go

--- user.go
+++ user.go
@@ -5,9 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"time"
-
-	"github.com/dgrijalva/jwt-go"
-	//"github.com/gorilla/mux"
+	"github.com/golang-jwt/jwt/v4"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -36,46 +34,30 @@ func InitialMigration() {
 		fmt.Println(err.Error())
 		panic("Cannot connect to the database")
 	} else {
-
 		fmt.Println("Connected to database successfully")
-
 	}
 	db.AutoMigrate(&User{})
 
 }
-/*func GetUsers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var users []User
-	db.Find(&users)
-	json.NewEncoder(w).Encode(users)
 
-}*/
 func GetUser(w http.ResponseWriter, r *http.Request) {
 	tokenStr := r.Header.Get("token")
-	fmt.Println(tokenStr)
 	claims := &Claims{}
 	tkn, err := jwt.ParseWithClaims(tokenStr, claims, func(t *jwt.Token) (interface{}, error) { return jwt_key, nil })
-	fmt.Println(err)
 	if err != nil {
-		//fmt.Println("inside1")
 		if err == jwt.ErrSignatureInvalid {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		} else {
-			//fmt.Println("inside2")
 			w.WriteHeader(http.StatusBadRequest)
 		}
 	}
 	if !tkn.Valid {
-		//fmt.Println("inside3")
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
-	//fmt.Println("inside4")
-	//w.Write([]byte(fmt.Sprintf("Hello, %s", claims.User)))
 	w.Header().Set("Content-Type", "application/json")
 	var user User
-	fmt.Println(claims.Email)
 	db.Where("email = ?", claims.Email).First(&user)
 	json.NewEncoder(w).Encode(&user)
 
@@ -88,23 +70,6 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(user)
 
 }
-/*func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var user User
-	params := mux.Vars(r)
-	db.Delete(&user, params["id"])
-	json.NewEncoder(w).Encode("The user was deleted successfully")
-}*/
-
-/*func UpdateUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var user User
-	params := mux.Vars(r)
-	db.First(&user, params["id"])
-	json.NewDecoder(r.Body).Decode(&user)
-	db.Save(&user)
-	json.NewEncoder(w).Encode(user)
-}*/
 func Login(w http.ResponseWriter, r *http.Request) {
 	var user User
 	var dBuser User
@@ -119,10 +84,8 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	db.Where("email = ?", email).First(&dBuser)
 	//if condition to check if any user exist with the email
 
-	//
-
 	expectedPassword := dBuser.Password
-	fmt.Println(pass1, expectedPassword)
+
 
 	if expectedPassword != pass1 {
 		w.WriteHeader(http.StatusUnauthorized)
@@ -140,11 +103,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	/*http.SetCookie(w,&http.Cookie{
-		Name: "token",
-		Value: tokenString,
-		Expires : expirationTime,
-	})*/
+	
 	result := make(map[string]string)
 
 	result["token"] = tokenString
@@ -154,37 +113,24 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(result)
 }
 
-
-	
-
 func Refresh(w http.ResponseWriter, r *http.Request) {
-
-	tokenStr := r.Header.Get("token")
+    tokenStr := r.Header.Get("token")
 
 	claims := &Claims{}
-
-	tkn, err := jwt.ParseWithClaims(tokenStr, claims,
-		func(t *jwt.Token) (interface{}, error) {
-			return jwt_key, nil
-		})
+	tkn, err := jwt.ParseWithClaims(tokenStr, claims, func(t *jwt.Token) (interface{}, error) { return jwt_key, nil })
 
 	if err != nil {
 		if err == jwt.ErrSignatureInvalid {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
 		}
-		w.WriteHeader(http.StatusBadRequest)
-		return
 	}
 	if !tkn.Valid {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
-
-	// if time.Unix(claims.ExpiresAt, 0).Sub(time.Now()) > 30*time.Second {
-	// 	w.WriteHeader(http.StatusBadRequest)
-	// 	return
-	// }
 
 	expirationTime := time.Now().Add(time.Minute * 5)
 
